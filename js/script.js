0df665e57567e9b3e1bb27ed2806e0720911a4aa
Vladimir Korobskiy <valmiki.main@gmail.com>
2023-05-08 13:43:57 +0300

    Seventh commit, add slider on main screen.

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.carousel_inner').slick({
+        speed: 1500,
+        autoplay: true, 
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/vector_2.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/vector_1.svg"></button>',
+    });
+  });
