503e3801736fe79e2fef1d230a922b29f5c34c53
Vladimir Korobskiy <valmiki.main@gmail.com>
2023-05-17 13:27:14 +0300

    Ninth commit, add full tabs, and it working

--- js/script.js
+++ js/script.js
@@ -6,4 +6,10 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/vector_2.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/vector_1.svg"></button>',
     });
+
+    $('ul.tabs').on('click', 'li:not(.tab_active)', function() {
+        $(this)
+          .addClass('tab_active').siblings().removeClass('tab_active')
+          .closest('div.container').find('div.carts_iphone').removeClass('carts_active').eq($(this).index()).addClass('carts_active');
+      });
   });
